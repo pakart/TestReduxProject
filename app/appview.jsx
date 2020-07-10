6bde9be23567fbf9e45c022d8cb78bd9ff18491f
pakart <obscurexdlol@gmail.com>
2020-07-10 16:50:35 +0300

    initial commit

---
+++ app/appview.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable react/no-string-refs */
+/* eslint-disable consistent-return */
+
+const React = require('react');
+const { connect } = require('react-redux');
+const actions = require('./actions.jsx');
+
+class PhoneForm extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+  }
+
+  onClick() {
+    if (this.refs.phoneInput.value !== '') {
+      const itemText = this.refs.phoneInput.value;
+      this.refs.phoneInput.value = '';
+      return this.props.addPhone(itemText);
+    }
+  }
+
+  render() {
+    return <div>
+            <input ref="phoneInput" />
+            <button onClick = {this.onClick.bind(this)}>Добавить</button>
+        </div>;
+  }
+}
+
+class PhoneItem extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>
+                <p>
+                    <b>{this.props.text}</b><br />
+                    <button onClick={() => this.props.deletePhone(this.props.text)}>Удалить</button>
+                </p>
+            </div>;
+  }
+}
+
+class PhonesList extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>
+        {this.props.phones.map((item) => <PhoneItem key={item}
+                    text={item}
+                    deletePhone={this.props.deletePhone}/>)}
+      </div>;
+  }
+}
+
+class AppView extends React.Component {
+  render() {
+    return <div>
+            <PhoneForm addPhone={this.props.addPhone}/>
+            <PhonesList {...this.props} />
+    </div>;
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    phones: state.get('phones'),
+  };
+}
+
+module.exports = connect(mapStateToProps, actions)(AppView);
